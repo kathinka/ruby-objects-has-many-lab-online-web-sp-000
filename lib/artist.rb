6bce00b457eac6311e91c0ea615ec12659899ef0
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-03-25 11:27:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,31 @@
 class Artist
-attr_accessor :name, :song
+attr_accessor :name
 @@all = []
 
   def initialize (name)
   @name = name
-  @songs = []
+  @all << self
   end
 
-def songs
-  @songs
-end
+  def self.all
+@@all
+  end
 
 def add_song(song)
-@songs << song
-@@all << song
 song.artist = self
 end
 
 def add_song_by_name(name)
 song = Song.new(name)
-@songs << song
-@@all << song
 song.artist = self
 end
 
-def self.song_count
-@@all.size
+def songs
+  Song.all.select{|song| song.artist==self}
 end
 
-
+def self.song_count
+Song.all.count
+end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,18 @@
 class Song
-attr_accessor :artist, :name 
-@@all = []
+  attr_accessor :artist, :name
+  @@all = []
 
-def initialize (name)
-@name = name
-@@all << self
-end
-
-def self.all
-@@all
-end
-
-def artist_name
-  if self.artist == nil
-    nil
-  else
-    self.artist.name
+  def initialize (name)
+  @name = name
+  @@all << self
   end
-end
+
+  def self.all
+  @@all
+  end
+
+  def artist_name
+  artist.name if artist
+  end
 
 end
